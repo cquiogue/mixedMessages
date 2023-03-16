a9dac965dd0da858ae4108e78a17439b51ce4f0c
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-03-16 16:43:37 -0400

    Added HTML and CSS to generate new message with each button click

--- script.js
+++ script.js
@@ -8,30 +8,29 @@ const ffMessages = {
     quote: ['Let\'s mosey', '...', 'This is my story', 'Worst birthday ever', 'I wanna be a blitzball!', 'Sharp errday']
 };
 
-let mergeMessages = [];
 let charName = " ";
 
-for (let prop in ffMessages) {
-    let index = randomNumberGenerator(ffMessages[prop].length);
-    switch (prop) {
-        case 'name':
-            mergeMessages.push(`${ffMessages[prop][index]} gets ready for battle.`);
-            charName = ffMessages[prop][index];
-            break;
-        case 'attack':
-            mergeMessages.push(`${charName} uses ${ffMessages[prop][index]}!`);
-            break;
-        case 'quote':
-            mergeMessages.push(`${charName} says, "${ffMessages[prop][index]}"`);
-            break;
-        default:
-            mergeMessages.push(`Not enough info`);
-    }
+const generateMessage = () => {
+  let mergeMessages = [];
+  for (let prop in ffMessages) {
+      let index = randomNumberGenerator(ffMessages[prop].length);
+      switch (prop) {
+          case 'name':
+              mergeMessages.push(`${ffMessages[prop][index]} gets ready for battle.`);
+              charName = ffMessages[prop][index];
+              break;
+          case 'attack':
+              mergeMessages.push(`${charName} uses ${ffMessages[prop][index]}!`);
+              break;
+          case 'quote':
+              mergeMessages.push(`${charName} says, "${ffMessages[prop][index]}"`);
+              break;
+          default:
+              mergeMessages.push(`Not enough info`);
+      }
+  }
+  const joined = mergeMessages.join('<br>');
+  document.getElementById("message-output").innerHTML = joined;
 };
 
-const displayMessages = message => {
-    const joined = mergeMessages.join('\n');
-    console.log(joined);
-}
-
-displayMessages(mergeMessages);
+document.getElementById("generate-button").addEventListener("click", generateMessage);
