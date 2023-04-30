06389005b1ef899b0aa3a13bf4c0c686f7868bd0
cquiogue <83323770+cquiogue@users.noreply.github.com>
2023-04-29 20:16:55 -0400

    added character sprites and changed some stylings

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-Final Fantasy Character Battle Quote Generator
-==============================================
+# Final Fantasy Character Battle Quote Generator
 
 Generates a random character, attack and win quote.
 
 ## Features:
+
 - Now has a webpage to display the messages from the JavaScript program.
 - Generates a new randomized message each time the button is clicked.
+- Generates character sprites to go along with each character.
 
 ## Upcoming:
+
 - Make the color scheme more fitting to Final Fantasy
-- Character sprites or avatars to go with each character
 - Messages displayed in a chat bubble
--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-const randomNumberGenerator = num =>{
-    return Math.floor(Math.random() * num);
+const randomNumberGenerator = num => {
+  return Math.floor(Math.random() * num);
 };
 
 const ffMessages = {
-    name: ['Cloud', 'Squall', 'Zidane', 'Tidus', 'Lightning', 'Noctis'],
-    attack: ['Omnislash', 'Lionheart', 'Blitz Ace', 'Army of One', 'Armiger'],
-    quote: ['Let\'s mosey', '...', 'This is my story', 'Worst birthday ever', 'I wanna be a blitzball!', 'Sharp errday']
+  name: ['Cloud', 'Squall', 'Zidane', 'Tidus', 'Lightning', 'Noctis'],
+  attack: ['Omnislash', 'Lionheart', 'Blitz Ace', 'Army of One', 'Armiger'],
+  quote: [
+    "Let's mosey",
+    '...',
+    'This is my story',
+    'Worst birthday ever',
+    'I wanna be a blitzball!',
+    'Sharp errday',
+  ],
+  imagePath: [
+    'images/cloud.png',
+    'images/squall.png',
+    'images/zidane.png',
+    'images/tidus.png',
+    'images/lightning.png',
+    'images/noctis.png',
+  ],
 };
 
-let charName = " ";
+const container = document.querySelector('.container');
+const messageDiv = document.querySelector('#message');
+const generateBtn = document.querySelector('#generate-btn');
+const charImage = document.querySelector('#char-image');
 
-const generateMessage = () => {
+generateBtn.addEventListener('click', () => {
   let mergeMessages = [];
+  let charName = ' ';
+  charImage.style.opacity = 0;
   for (let prop in ffMessages) {
-      let index = randomNumberGenerator(ffMessages[prop].length);
-      switch (prop) {
-          case 'name':
-              mergeMessages.push(`${ffMessages[prop][index]} gets ready for battle.`);
-              charName = ffMessages[prop][index];
-              break;
-          case 'attack':
-              mergeMessages.push(`${charName} uses ${ffMessages[prop][index]}!`);
-              break;
-          case 'quote':
-              mergeMessages.push(`${charName} says, "${ffMessages[prop][index]}"`);
-              break;
-          default:
-              mergeMessages.push(`Not enough info`);
-      }
+    let index = randomNumberGenerator(ffMessages[prop].length);
+    switch (prop) {
+      case 'name':
+        mergeMessages.push(`${ffMessages[prop][index]} gets ready for battle.`);
+        charName = ffMessages[prop][index];
+        break;
+      case 'attack':
+        mergeMessages.push(`${charName} uses ${ffMessages[prop][index]}!`);
+        break;
+      case 'quote':
+        mergeMessages.push(`${charName} says, "${ffMessages[prop][index]}"`);
+        break;
+      case 'imagePath':
+        charImage.src =
+          ffMessages['imagePath'][ffMessages['name'].indexOf(charName)];
+        break;
+      default:
+        mergeMessages.push(`Not enough info`);
+    }
   }
-  const joined = mergeMessages.join('<br>');
-  document.getElementById("message-output").innerHTML = joined;
-};
-
-document.getElementById("generate-button").addEventListener("click", generateMessage);
+  const joined = mergeMessages.join('<br><br>');
+  messageDiv.innerHTML = joined;
+  charImage.style.opacity = 1;
+});
